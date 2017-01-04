8a789b54f30206d928c96d182abdfe3ff38e4a6a
Heesang Chae <theand@gmail.com>
2017-01-04 11:22:37 +0900

    comment out all. moved to run_manually.sh at boxen.

--- brew.sh
+++ brew.sh
@@ -3,21 +3,21 @@
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
-brew update
+#brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade
+#brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+#brew install coreutils
 
 # Install some other useful utilities like `sponge`.
 # brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 #brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+#brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
@@ -26,10 +26,10 @@ brew install gnu-sed --with-default-names
 #brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+#   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+#   chsh -s /usr/local/bin/bash;
+# fi;
 
 # Install `wget` with IRI support.
 #brew install wget --with-iri
@@ -41,7 +41,7 @@ fi;
 #brew install narwhal
 
 # Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
+# brew install vim --override-system-vi
 #brew install homebrew/dupes/grep
 #brew install homebrew/dupes/openssh
 #brew install homebrew/dupes/screen
@@ -102,4 +102,4 @@ brew install vim --override-system-vi
 #brew install zopfli
 
 # Remove outdated versions from the cellar.
-brew cleanup
+#brew cleanup
