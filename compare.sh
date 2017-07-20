b192378107d2f5ad95caa2321207d7bf456acd28
Heesang Chae <theand@gmail.com>
2017-07-20 13:46:41 +0900

    compapre current working set with repo set using bcomp

---
+++ compare.sh
@@ -0,0 +1,26 @@
+#!/usr/local/bin/bash
+
+
+bcomp ../.aliases ./.aliases
+bcomp ../.bash_profile ./.bash_profile
+bcomp ../.bash_prompt ./.bash_prompt
+bcomp ../.bashrc ./.bashrc
+bcomp ../.curlrc ./.curlrc
+bcomp ../.editorconfig ./.editorconfig
+bcomp ../.exports ./.exports
+bcomp ../.functions ./.functions
+bcomp ../.fzf.bash ./.fzf.bash
+bcomp ../.gdbinit ./.gdbinit
+bcomp ../.gemrc ./.gemrc
+bcomp ../.gitattributes ./.gitattributes
+bcomp ../.gitconfig ./.gitconfig
+bcomp ../.gitignore ./.gitignore
+bcomp ../.gvimrc ./.gvimrc
+bcomp ../.hgignore ./.hgignore
+bcomp ../.hushlogin ./.hushlogin
+bcomp ../.inputrc ./.inputrc
+bcomp ../.path ./.path
+bcomp ../.profile ./.profile
+bcomp ../.screenrc ./.screenrc
+bcomp ../.tmux.conf ./.tmux.conf
+bcomp ../.wgetrc ./.wgetrc
