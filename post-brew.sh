29085ea9a8bf2e15efcf3ff886ea899f531031cb
Heesang Chae <theand@gmail.com>
2019-04-30 19:47:57 +0900

    Brewfile 설치한 다음에 처리해야할 것들.

---
+++ post-brew.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+#Post process after brew bundle all installed
+
+set -e
+
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
